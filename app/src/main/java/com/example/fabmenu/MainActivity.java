2904b3d7d5a7d1c69d0491897255d97683507a21
trulymittal <trulymittal@gmail.com>
2018-06-27 02:14:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fabmenu/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.fabmenu;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    FloatingActionButton fabMain, fabOne, fabTwo, fabThree;
+    Float translationY = 100f;
+
+    OvershootInterpolator interpolator = new OvershootInterpolator();
+
+    private static final String TAG = "MainActivity";
+
+    Boolean isMenuOpen = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFabMenu();
+
+    }
+
+    private void initFabMenu() {
+        fabMain = findViewById(R.id.fabMain);
+        fabOne = findViewById(R.id.fabOne);
+        fabTwo = findViewById(R.id.fabTwo);
+        fabThree = findViewById(R.id.fabThree);
+
+        fabOne.setAlpha(0f);
+        fabTwo.setAlpha(0f);
+        fabThree.setAlpha(0f);
+
+        fabOne.setTranslationY(translationY);
+        fabTwo.setTranslationY(translationY);
+        fabThree.setTranslationY(translationY);
+
+        fabMain.setOnClickListener(this);
+        fabOne.setOnClickListener(this);
+        fabTwo.setOnClickListener(this);
+        fabThree.setOnClickListener(this);
+    }
+
+    private void openMenu() {
+        isMenuOpen = !isMenuOpen;
+
+        fabMain.animate().setInterpolator(interpolator).rotation(45f).setDuration(300).start();
+
+        fabOne.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        fabTwo.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        fabThree.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+
+
+    }
+
+    private void closeMenu() {
+        isMenuOpen = !isMenuOpen;
+
+        fabMain.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start();
+
+        fabOne.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        fabTwo.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        fabThree.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+
+    }
+
+    private void handleFabOne() {
+        Log.i(TAG, "handleFabOne: ");
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        
+
+        switch (view.getId()) {
+            case R.id.fabMain:
+                Log.i(TAG, "onClick: fab main");
+                if (isMenuOpen) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
+                break;
+            case R.id.fabOne:
+                Log.i(TAG, "onClick: fab one");
+                handleFabOne();
+                if (isMenuOpen) {
+                    closeMenu();
+                } else {
+                    openMenu();
+                }
+                break;
+            case R.id.fabTwo:
+                Log.i(TAG, "onClick: fab two");
+                break;
+            case R.id.fabThree:
+                Log.i(TAG, "onClick: fab three");
+                break;
+        }
+
+    }
+}
